ebcb04c3c9c998c62e809fdfb6e824317a2583e7
beatrizfteixeira <biaft03@protonmail.com>
2023-06-20 16:02:02 -0300

    spring boot dev-tools automatically reloads application

--- src/main/java/com/biaft/springAppBia/rest/RestControllerInitial.java
+++ src/main/java/com/biaft/springAppBia/rest/RestControllerInitial.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class RestControllerInitial {
     @GetMapping("/")
     public String sayHello() {
-        return "Hello worldao";
+        return "Hello worlda553o";
     }
 
 }
