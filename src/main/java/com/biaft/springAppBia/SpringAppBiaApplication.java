ebcbe08d13c6364a6523a67bff392c87a50f90a5
beatrizfteixeira <biaft03@protonmail.com>
2023-06-20 15:17:43 -0300

    rest controller

---
+++ src/main/java/com/biaft/springAppBia/SpringAppBiaApplication.java
@@ -0,0 +1,13 @@
+package com.biaft.springAppBia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppBiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppBiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biaft/springAppBia/SpringAppBiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biaft.springAppBia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppBiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
