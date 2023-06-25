2998a6f43f58098b82708a1a8d1d0451ad9567c2
beatrizfteixeira <biaft03@protonmail.com>
2023-06-25 18:07:54 -0300

    saving student

--- CRUD/cruddemo/src/main/java/beatrizfteixeira/cruddemo/CruddemoApplication.java
+++ CRUD/cruddemo/src/main/java/beatrizfteixeira/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package beatrizfteixeira.cruddemo;
 
+import beatrizfteixeira.cruddemo.entity.Student;
+import beatrizfteixeira.cruddemo.dao.StudentDAO;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +15,15 @@ public class CruddemoApplication {
 		System.out.println("hellorr!");
 	}
 	@Bean
-	public CommandLineRunner commandLineRunner(String args[]) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			System.out.println("hello wordao");
+			createStudent(studentDAO);
 		};
 	}
 
+	public void createStudent(StudentDAO studentDAO) {
+		Student student = new Student("Claudette", "Gregotica", "clau@email.com");
+		studentDAO.save(student);
+		student.toString();
+	}
 }
---
+++ CRUD/cruddemo/src/main/java/beatrizfteixeira/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,11 @@
+package beatrizfteixeira.cruddemo.dao;
+
+import beatrizfteixeira.cruddemo.entity.Student;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public interface StudentDAO {
+
+    public void save(Student student);
+
+}
